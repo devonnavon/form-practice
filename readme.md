d2cbe438f0777007b2ef26a0fee752c621bd8729
Devon Navon <devon.navon@gmail.com>
2020-06-03 17:46:32 -0700

    done thanks

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Project
+
+Build a browser form which collects Email, Country, Zip Code, Password and Password Confirmation fields. It should use live inline validation to inform the user whether a field is properly filled in or not. That means highlighting a field red and providing a helpful error message until it has been filled in properly.
+
+The form doesn’t need to actually submit, but you should give an error message if the button is pushed with any active errors or unfilled required fields. If all is well and the form is “submitted”, give the user a high five.
+
+1. Set up a blank HTML document
+2. Think about how you would set up the different form elements and their accompanying validators. What objects and functions will you need? A few minutes of thought can save you from wasting an hour of coding. The best thing you can do is whiteboard the entire solution before even touching the computer.
+3. Write the simple form elements. Don’t worry about styling them.
+4. Add the JavaScript code that checks validation as the user progresses through the form. When a user leaves a form field it should automatically validate that field.
+5. Test out all possible cases.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
-console.log('lord');
+import countries from 'country-list';
+
+console.log(typeof countries.getNames()[0]);
+
+const createOption = (value) => {
+	let option = document.createElement('OPTION');
+	option.innerHTML = value;
+	option.setAttribute('value', value);
+	return option;
+};
+
+const selectCountry = document.getElementById('country');
+
+countries.getNames().forEach((country) => {
+	selectCountry.appendChild(createOption(country));
+});
+
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+// const form = document.getElementsByTagName('form')[0];
+
+const zipCode = document.getElementById('zipcode');
+const zipCodeError = document.querySelector('#zipcode + span.error');
+
+zipCode.addEventListener('input', (e) => {
+	if (zipCode.validity.valid) {
+		zipCodeError.innerHTML = ''; // Reset the content of the message
+		zipCodeError.className = 'error'; // Reset the visual state of the message
+	} else {
+		// If there is still an error, show the correct error
+		if (zipCode.validity.patternMismatch) {
+			zipCodeError.textContent = 'enter a number PLEASE';
+		} else if (zipCode.validity.tooShort || zipCode.validity.tooLong) {
+			zipCodeError.textContent = `Zipcode should be ${zipCode.minLength} characters; you entered ${zipCode.value.length}.`;
+		}
+		zipCodeError.className = 'error active';
+	}
+});
+
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+
+const passwordError = document.querySelector('#confirmPassword + span.error');
+
+confirmPassword.addEventListener('input', (e) => {
+	if (password.value == confirmPassword.value) {
+		passwordError.innerHTML = '';
+		passwordError.className = 'error';
+	} else {
+		passwordError.textContent = 'Your passwords do not match';
+		passwordError.className = 'error active';
+	}
+});
+
+const email = document.getElementById('email');
+const emailError = document.querySelector('#email + span.error');
+
+email.addEventListener('input', function (event) {
+	// Each time the user types something, we check if the
+	// form fields are valid.
+
+	if (email.validity.valid) {
+		// In case there is an error message visible, if the field
+		// is valid, we remove the error message.
+		emailError.innerHTML = ''; // Reset the content of the message
+		emailError.className = 'error'; // Reset the visual state of the message
+	} else {
+		// If there is still an error, show the correct error
+		showEmailError();
+	}
+});
+
+function showEmailError() {
+	if (email.validity.valueMissing) {
+		// If the field is empty
+		// display the following error message.
+		emailError.textContent = 'You need to enter an e-mail address.';
+	} else if (email.validity.typeMismatch) {
+		// If the field doesn't contain an email address
+		// display the following error message.
+		emailError.textContent = 'Entered value needs to be an e-mail address.';
+	} else if (email.validity.tooShort) {
+		// If the data is too short
+		// display the following error message.
+		emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+	}
+
+	// Set the styling appropriately
+	emailError.className = 'error active';
+}
